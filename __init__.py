54a4eb2bab1bef3ab832781f8153fb3ae49f2625
imk-asyk <2300011813@stu.pku.edu.cn>
2025-07-21 16:26:38 +0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,23 @@
+"""Author: imk-asyk
+This is a Python tool to import 
+"""
+
+import shutil
+import os
+
+
+def treeimport():
+  "Import all files in a package folder."
+
+
+def treeimport_builtins():
+  "Import all built-in modules."
+
+def treeimport_original():
+  "Import all original modules."
+
+
+def treeimport_all():
+  """DANGEROUS! THINK TWICE BEFORE CALLING THIS FUNCTION!
+Import all packages found in your Lib. May cause your code stuck.
+Some original packages in Python will output something in stdout."""
